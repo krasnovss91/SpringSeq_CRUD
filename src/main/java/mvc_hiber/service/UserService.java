0e9afbfdec18ac939cb9e8607fe1c42324bbed8e
krasnovss91 <krasnovstanislav91@gmail.com>
2020-08-03 19:29:29 +0400

    Commit 3

--- src/main/java/mvc_hiber/service/UserService.java
+++ src/main/java/mvc_hiber/service/UserService.java
@@ -9,4 +9,12 @@ import java.util.List;
 public interface UserService {
 
     List<User> getAllUsers();
+    //static UserService getInstance();
+    boolean checkUserByName(String name);
+    User getUserById(Long id);
+    boolean checkUserByLogin(String login);
+    void addUser(User user);
+    void deleteUserById(Long id);
+    void updateUser(User user);
+    User isExist(String login, String password);
 }
--- src/main/java/mvc_hiber/service/UserServiceImpl.java
+++ src/main/java/mvc_hiber/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import mvc_hiber.model.User;
 import mvc_hiber.dao.UserDao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Service("userService")
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
+    private static UserService userService;
 
     @Autowired
     public void setUserDao(UserDao userDao) {
@@ -22,7 +24,82 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
+  /*  public List<User> getAllUsers() {
+        return this.userDao.getAllUsersDao();
+    } */
+
+    public static UserService getInstance() {
+        if (userService == null) {
+            userService = new UserService(new UserDaoFactory().getUserDao());
+        }
+        return userService;
+    }
+
+
+    public User getUserById(Long id) {
+        User user = null;
+        try {
+            user = userDao.getUserByIdDao(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
     public List<User> getAllUsers() {
-        return this.userDao.getAllUsers();
+        return userDao.getAllUsersDao();
+    }
+
+    public boolean checkUserByName(String name) {
+
+        try {
+            userDao.checkUserByNameDao(name);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean checkUserByLogin(String login) {
+
+        try {
+            return userDao.checkUserByLoginDao(login);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void addUser(User user) {
+        if (user.getName() != null && user.getName().length() > 0
+                && user.getLogin() != null && user.getLogin().length() > 0
+                && user.getPassword() != null && user.getPassword().length() > 0)
+
+            userDao.addUserDao(user);
+    }
+
+
+    public void deleteUserById(Long id) {
+
+        try {
+            userDao.deleteUserByIdDao(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user) {
+        if (user.getName() != null && user.getName().length() > 0
+                && user.getLogin() != null && user.getLogin().length() > 0
+                && user.getPassword() != null && user.getPassword().length() > 0) {
+
+            userDao.updateUserDao(user);
+        }
+    }
+
+    public User isExist(String login, String password) {
+        User user = null;
+        user = userDao.isExist(login, password);
+        return user;
     }
 }
