1f13fdeee3b008b80ceba42e825538f2a091fce7
krasnovss91 <krasnovstanislav91@gmail.com>
2020-09-02 18:13:21 +0400

    Update

--- src/main/java/mvc_hiber/config/PersistenceJPAConfig.java
+++ src/main/java/mvc_hiber/config/PersistenceJPAConfig.java
@@ -81,7 +81,8 @@ public class PersistenceJPAConfig {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+       // properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");//избавиться от create-drop
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty(
                 "hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 
