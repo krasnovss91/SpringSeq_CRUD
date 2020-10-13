80868789c660a931c1a0657957bc6277d3317bb1
krasnovss91 <krasnovstanislav91@gmail.com>
2020-10-13 19:13:46 +0400

    Granted_Autority

--- src/main/java/mvc_hiber/model/GrantedAutority.java
+++ src/main/java/mvc_hiber/model/GrantedAutority.java
@@ -1,4 +1,17 @@
 package mvc_hiber.model;
 
+import java.util.Set;
+
 public interface GrantedAutority {
+    Long getId();
+
+    void setId(Long id);
+
+    String getName();
+
+    void setName(String name);
+
+    Set<User> getUsers(Set<User> users);
+
+    String toString();
 }
