b234828fc8598a6babed7a29a83044686530dfee
krasnovss91 <krasnovstanislav91@gmail.com>
2020-10-16 18:14:31 +0400

    RSA_Key_1

--- src/main/java/mvc_hiber/config/SpringSecurityConfig.java
+++ src/main/java/mvc_hiber/config/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.converter.RsaKeyConverters;
 
 
 
